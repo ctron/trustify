57825a47a35d4856376dff7e905e71c0834f10c5
github-action-benchmark <github@users.noreply.github.com>
2024-11-08 15:23:04 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for e1096f8d40baf0fb36ef4a9061728642c70d10f7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1730973077633,
+  "lastUpdate": 1731079384881,
   "repoUrl": "https://github.com/ctron/trustify",
   "entries": {
     "Benchmark": [
@@ -260,6 +260,35 @@ window.BENCHMARK_DATA = {
           {
             "name": "Ingest DS3",
             "value": 22,
+            "unit": "s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "ctron@dentrassi.de",
+            "name": "Jens Reimann",
+            "username": "ctron"
+          },
+          "committer": {
+            "email": "00hf11@gmail.com",
+            "name": "Helio Frota",
+            "username": "helio-frota"
+          },
+          "distinct": false,
+          "id": "e1096f8d40baf0fb36ef4a9061728642c70d10f7",
+          "message": "chore: prepare release 0.1.0-alpha.22",
+          "timestamp": "2024-11-07T10:10:36Z",
+          "tree_id": "eada5b0ce09a210e5ef349b929f1843280d4f6fd",
+          "url": "https://github.com/ctron/trustify/commit/e1096f8d40baf0fb36ef4a9061728642c70d10f7"
+        },
+        "date": 1731079384217,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Ingest DS3",
+            "value": 23,
             "unit": "s"
           }
         ]
