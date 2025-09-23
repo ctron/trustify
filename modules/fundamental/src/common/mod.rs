354e5adee2ebfaa7a49f5c9943df5a4420755656
Jens Reimann <ctron@dentrassi.de>
2025-09-23 17:15:46 +0200

    feat(fundamental): return all scores in addition for sbom advisories

--- modules/fundamental/src/common/mod.rs
+++ modules/fundamental/src/common/mod.rs
@@ -5,6 +5,7 @@ use serde::{Deserialize, Serialize};
 use trustify_entity::sbom_package_license::LicenseCategory;
 use utoipa::ToSchema;
 
+pub mod model;
 pub mod service;
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, ToSchema, FromQueryResult)]
