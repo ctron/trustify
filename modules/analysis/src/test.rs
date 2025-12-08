1c0cf787d0f068d60c6c365e1f5aa4e4d7a55cfb
Jens Reimann <ctron@dentrassi.de>
2025-12-08 10:48:34 +0100

    test(analysis): create a reproducer test for TC-3286
    TC-3286 is about the fact that processing of `q` works differently for
    the DB implementation than it does for the in-memory implementation.
    This lead to weird behavior in the past. Both implementations must be
    aligned to prevent this in the future.
    
    This test gives an example. The test is expected to fail with the
    current codebase.

--- modules/analysis/src/test.rs
+++ modules/analysis/src/test.rs
@@ -112,3 +112,12 @@ where
 
     f(ancestors.as_slice())
 }
+
+/// escape values for putting them into a `q` parameter
+pub fn escape_q(q: impl AsRef<str>) -> String {
+    let q = q.as_ref();
+
+    q.replace('\\', "\\\\")
+        .replace('&', "\\&")
+        .replace('=', "\\=")
+}
